d6ae376a4a20d44ffdfb8a471c82676260567e13
Ho Jang <61655622+naraguho@users.noreply.github.com>
2026-02-05 12:11:09 -0500

    Update data_generation.jl

--- data/data_generation.jl
+++ data/data_generation.jl
@@ -65,7 +65,7 @@ function hopping_Hamiltonian_1D(dim::Int)
         H[i, j] = -1
     end
     return H + H'
-end
+    end
 
 onsite_Hamiltonian_1D(dim, Q) = -Diagonal(Q)
 compute_dX(Q, P, r, n) = r .* P
